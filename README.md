e76d06d3cfe0cee6c9caffaf70331d5af431413d
Naseband <17707492+Naseband@users.noreply.github.com>
2024-10-12 15:51:39 +0200

    Add more hide options
    Added more hide methods and general cleanup
    
    - Can now choose between destroying the window, hiding the window and disabling window input
    - Log is now cleared on startup
    - Adjusted sleep timings (it was possible to miss window popups before)

--- README.md
+++ README.md
@@ -4,36 +4,48 @@ Kills Windows 10's annoying Volume and Brightness display until the next time ex
 
 I only made this version as it is a lot simpler and I do not ever want to restore it. I also used a slightly different method to bring up the OSD (no key sends to the active window).
 
-# Launch options
+# Command Line arguments
 
-There are three available command line parameters:
+|   **Option**   | **Effect** |
+|----------------|---------------|
+| `-debug`     | Logs debug messages to log.txt |
+| `-delay`     | Uses an initial delay of 5 seconds (useful on Windows startup) |
+| `-daemon`    | Keeps VolumeOSD running in the background. This prevents the OSD from showing again after explorer is restarted. Note that the OSD is only shown once on startup. If the OSD returns, a user-interaction is required for it to show again. |
 
-"-debug" will enable logging to log.txt.
+# Hide Methods
 
-"-delay" will delay the launch by five seconds.
+The following hide methods are also set via command line arguments.
+Only one of them can be used at a time.
 
-"-daemon" will keep the program running in the background. This will keep the program looking for the OSD in case you ever restart explorer.exe.
-Note that it does not attempt to bring up the OSD a second time after initial launch, instead it will hide the OSD whenever it is shown via user interaction.
+|   **Method**   | **Effect**|
+|----------------|------------|
+| `-destroy` | (default) Destroys the window completely. Side effect: App Commands (like multimedia inputs) do not propagate (ie. Browser media playback). |
+| `-hide` | Hides the window like in HideVolumeOSD and disables its keyboard and mouse input (just in case it does return). |
+| `-noinput` | The window will show as usual, but keyboard and mouse input will be disabled. No accidental volume increase! |
 
-You can launch this program at startup either by adding a shortcut to your startup directory (Win + R, "shell:startup") or create a task in task scheduler for individual users (Win + R, "taskschd.msc").
+# Launching
+
+You can launch this program at startup either by adding a shortcut to your startup directory (Win + R, "shell:startup") or creating a task in task scheduler for individual users (Win + R, "taskschd.msc").
 
 When using a task, it's usually best to add a log on event trigger. There is a configurable delay aswell.
 
-The audio files in the release are optional, you can remove them if you want this program to be silent.
+The audio files in the release are optional, you can leave them away if you want this program to be silent.
 
 # Known issues
 
-When the OSD is hidden with the method this tool uses, media keys will not be propagated through the Windows 10 multimedia interface.
+When the OSD is hidden with the default method, media keys will not be propagated through the Windows 10 multimedia interface.
 
-This is not a problem for most media players like Spotify, Winamp, VLC, etc, however some applications rely on that interface exclusively (such as Firefox).
+This is not a problem for most media players like Spotify, Winamp or VLC, however some applications rely on that interface exclusively (such as Firefox).
+
+If you want to keep that interface working, use -hide or -noinput together with the -daemon option.
 
 # Building
 
 The project was created with VS2019 and requires C++17 or C++20 to build.
 
-# Credits
+# Thanks
 
-Credits to Marcus Venturi for finding the method used to detect the window.
+Thanks to Marcus Venturi for finding the method used to detect the window.
 
 https://github.com/UnlimitedStack/HideVolumeOSD
 
--- VolumeOSD.cpp
+++ VolumeOSD.cpp
@@ -6,48 +6,127 @@
 
 #pragma comment(lib, "Winmm.lib") // for audio
 
-using namespace std;
+// ------------------------------------------------------------ 
 
-bool g_bDebug = false;
+enum class eHideMethod
+{
+    DESTROY,    // Destroy the window. Never shows again unless explorer.exe is restarted.
+    HIDE,       // Hides the window like in HideVolumeOSD, but may occasionally bug out (should to be run in daemon mode).
+    NOINPUT     // Show the window as usual, but prevent any mouse or keyboard interaction.
+};
 
-int ShowError(wstring Text)
+enum class eHideResult
+{
+    FAIL,       // Plays failure sound and shows error.
+    SUCCESS,    // Plays success sound.
+    NOCHANGE    // Does nothing.
+};
+
+constexpr const wchar_t* GetHideMethodName(eHideMethod method)
+{
+    switch (method)
+    {
+    case eHideMethod::DESTROY:
+        return L"Destroy";
+
+    case eHideMethod::HIDE:
+        return L"Hide";
+
+    case eHideMethod::NOINPUT:
+        return L"No Input";
+    }
+
+    return L"Unknown";
+}
+
+bool g_bDebug{ false };
+
+std::wofstream g_LogFile(L"log.txt", std::ios::out);
+
+// ------------------------------------------------------------ 
+
+int ShowError(const std::wstring& text)
 {
     mciSendString(L"open \"notify-error.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
     mciSendString(L"play mp3 wait", NULL, 0, NULL);
     mciSendString(L"close mp3", NULL, 0, NULL);
 
-    MessageBox(NULL, Text.c_str(), L"KillVolumeOSD", 0);
+    MessageBox(NULL, text.c_str(), L"KillVolumeOSD", 0);
 
     return 1;
 }
 
-bool HideWindow(HWND hWnd)
-{
-    return PostMessage(hWnd, WM_DESTROY, 0, 0);
-}
-
-void Log(wstring Text)
+void Log(const std::wstring& text)
 {
     if (!g_bDebug)
         return;
 
-    wofstream File(L"log.txt", ios::app);
-
-    if (File.is_open())
-        File << Text << endl;
+    g_LogFile << text << std::endl;
 }
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+eHideResult HideWindow(HWND window, eHideMethod method)
+{
+    switch (method)
+    {
+    case eHideMethod::DESTROY:
+    {
+        if (PostMessageW(window, WM_DESTROY, 0, 0))
+            return eHideResult::SUCCESS;
+        else
+            return eHideResult::FAIL;
+    }
+
+    case eHideMethod::HIDE:
+    {
+        auto style{ GetWindowLongA(window, GWL_STYLE) };
+
+        if (style & (WS_ICONIC | WS_DISABLED))
+            return eHideResult::NOCHANGE;
+
+        if (style == 0)
+            return eHideResult::FAIL;
+
+        // WS_ICONIC means the window is minimized when shown.
+        // Also set WS_DISABLED so in the rare case it does pop up it's not clickable.
+        SetWindowLongA(window, GWL_STYLE, style | (WS_ICONIC | WS_DISABLED));
+
+        return eHideResult::SUCCESS;
+    }
+
+    case eHideMethod::NOINPUT:
+    {
+        auto style{ GetWindowLongA(window, GWL_STYLE) };
+
+        if (style & WS_DISABLED)
+            return eHideResult::NOCHANGE;
+
+        if (style == 0)
+            return eHideResult::FAIL;
+
+        // Setting WS_DISABLED doesn't hide it, but prevents any input from being processed.
+        SetWindowLongA(window, GWL_STYLE, style | WS_DISABLED);
+        
+        return eHideResult::SUCCESS;
+    }
+    }
+
+    // May work if setting the initial window pos (but it doesn't as-is):
+    // SetWindowPos(window, NULL, -10000, 0, 0, 0, SWP_NOACTIVATE | SWP_NOSIZE | SWP_NOZORDER | SWP_NOSENDCHANGING);
+
+    return eHideResult::NOCHANGE;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
     hPrevInstance;
 
-    wstring Params{ lpCmdLine };
+    std::wstring params{ lpCmdLine };
 
-    bool bDaemon = false;
-    bool bDelay = false;
+    bool is_daemon{ false };
+    bool use_delay{ false };
+    eHideMethod method{ eHideMethod::DESTROY };
+
+    // Parse command line args
 
 #if defined _DEBUG
 
@@ -55,102 +134,121 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 #else
 
-    if (Params.find(L"-debug") != Params.npos)
+    if (params.find(L"-debug") != params.npos)
         g_bDebug = true;
 
 #endif
 
-    if (Params.find(L"-daemon") != Params.npos)
-        bDaemon = true;
+    if (params.find(L"-daemon") != params.npos)
+        is_daemon = true;
 
-    if (Params.find(L"-delay") != Params.npos)
-        bDelay = true;
+    if (params.find(L"-delay") != params.npos)
+        use_delay = true;
 
-    Log(L"--- Killing VolumeOSD");
-    Log(format(L"-- Debug: {}", g_bDebug ? L"Yes" : L"No"));
-    Log(format(L"-- Delay: {}", bDelay ? L"Yes" : L"No"));
-    Log(format(L"-- Daemon: {}", bDaemon ? L"Yes" : L"No"));
+    if (params.find(L"-destroy") != params.npos)
+        method = eHideMethod::DESTROY;
+    else if (params.find(L"-hide") != params.npos)
+        method = eHideMethod::HIDE;
+    else if (params.find(L"-noinput") != params.npos)
+        method = eHideMethod::NOINPUT;
 
-    HWND hWndExplorer;
+    Log(L"-- Killing VolumeOSD");
+    Log(std::format(L"- Debug: {}", g_bDebug ? L"Yes" : L"No"));
+    Log(std::format(L"- Daemon: {}", is_daemon ? L"Yes" : L"No"));
+    Log(std::format(L"- Delay: {}", use_delay ? L"Yes" : L"No"));
+    Log(std::format(L"- Method: {}", GetHideMethodName(method)));
+
+    // Get the explorer window handle
+
+    HWND window_explorer{ NULL };
 
     do
     {
         Sleep(200);
 
-        hWndExplorer = GetShellWindow();
+        window_explorer = GetShellWindow();
     }
-    while (hWndExplorer == NULL);
+    while (window_explorer == NULL);
 
-    Log(format(L"GetShellWindow HWND = {}", (void*)hWndExplorer));
+    Log(std::format(L"GetShellWindow HWND = {}", (void*)window_explorer));
 
-    if (bDelay)
+    // Delay
+
+    if (use_delay)
         Sleep(5000);
 
-    PostMessage(hWndExplorer, WM_APPCOMMAND, 0, APPCOMMAND_VOLUME_MUTE << 16);
-    PostMessage(hWndExplorer, WM_APPCOMMAND, 0, APPCOMMAND_VOLUME_MUTE << 16);
+    // Show the VolumeOSD (only once)
 
-    HWND hWndTarget;
+    Log(L"-- Sending Mute Key messages ...");
+
+    PostMessage(window_explorer, WM_APPCOMMAND, 0, APPCOMMAND_VOLUME_MUTE << 16);
+    PostMessage(window_explorer, WM_APPCOMMAND, 0, APPCOMMAND_VOLUME_MUTE << 16);
+
+    // Run main loop - only runs once if is_daemon is false
+
+    HWND window_target{ NULL };
 
     do
     {
         do
         {
-            Sleep(700);
+            HWND window_parent{ NULL };
 
-            HWND hWndParent = NULL;
+            window_target = NULL;
 
-            hWndTarget = NULL;
-
-            while ((hWndParent = FindWindowEx(NULL, hWndParent, L"NativeHWNDHost", L"")) != NULL)
+            while ((window_parent = FindWindowEx(NULL, window_parent, L"NativeHWNDHost", L"")) != NULL)
             {
-                Log(format(L"Parent candidate window HWND = {}", (void*)hWndParent));
+                Log(std::format(L"Parent candidate window HWND = {}", (void*)window_parent));
 
-                if (FindWindowExW(hWndParent, NULL, L"DIRECTUIHWND", L"") != NULL)
+                if (FindWindowExW(window_parent, NULL, L"DIRECTUIHWND", L"") != NULL)
                 {
-                    Log(L"Found child window");
-
-                    if (hWndTarget == NULL)
+                    if (window_target == NULL)
                     {
-                        Log(format(L"Target HWND = {}", (void*)hWndParent));
+                        Log(std::format(L"Target HWND = {}", (void*)window_parent));
 
-                        hWndTarget = hWndParent;
+                        window_target = window_parent;
                     }
                 }
             }
+
+            if(window_target == NULL)
+                Sleep(500);
         }
-        while (hWndTarget == NULL);
+        while (window_target == NULL);
 
-        if (hWndTarget != NULL)
+        // Only do something if the window is actually visible
+
+        if (IsWindowVisible(window_target))
         {
-            Log(format(L"Target window belongs to PID {}", GetWindowThreadProcessId(hWndTarget, 0)));
-
-            Log(format(L"Window is visible: {}", IsWindowVisible(hWndTarget) ? L"Yes" : L"No"));
-
-            if (IsWindowVisible(hWndTarget))
+            switch (HideWindow(window_target, method))
             {
-                if (HideWindow(hWndTarget))
-                {
-                    Log(L"VolumeOSD Window hidden successfully");
+            case eHideResult::SUCCESS:
+                Log(L"VolumeOSD Window hidden successfully");
 
-                    mciSendString(L"open \"notify-success.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
-                    mciSendString(L"play mp3 wait", NULL, 0, NULL);
-                    mciSendString(L"close mp3", NULL, 0, NULL);
-                }
-                else
-                {
-                    Log(L"Failed to hide VolumeOSD window");
+                mciSendString(L"open \"notify-success.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
+                mciSendString(L"play mp3 wait", NULL, 0, NULL);
+                mciSendString(L"close mp3", NULL, 0, NULL);
+                break;
 
-                    ShowError(L"Failed to hide VolumeOSD window.");
-                }
+            case eHideResult::FAIL:
+                Log(L"Failed to hide VolumeOSD window");
+
+                ShowError(L"Failed to hide VolumeOSD window.");
+                break;
             }
         }
 
-        if (bDaemon)
+        if (is_daemon)
+        {
             Log(L"-- Waiting for next check");
-
-        Sleep(3000);
+            Sleep(2500);
+        }
     }
-    while (bDaemon);
+    while (is_daemon);
+
+    Sleep(500);
 
     return 0;
 }
+
+// ------------------------------------------------------------ EOF
